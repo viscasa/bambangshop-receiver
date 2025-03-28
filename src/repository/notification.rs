50581be6867b05528ebe7d44cdf73833b795bfff
Attar <attaralwie@gmail.com>
2025-03-28 13:18:22 +0700

    Implement add function in Subscriber repository.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -10,4 +10,9 @@ lazy_static! {
 pub struct NotificationRepository;
 
 impl NotificationRepository {
+    pub fn add(notification: Notification) -> Notification {
+        NOTIFICATIONS.write().unwrap()
+            .push(notification.clone());
+        return notification;
+    }
 }
